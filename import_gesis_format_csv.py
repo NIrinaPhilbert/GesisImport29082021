d14ddb390dfb304b0b4465bab4e9caa6e238e673
NIrinaPhilbert <randriamiaranirina@gmail.com>
2021-09-02 16:10:30 +0300

    pb delai 10 s

--- import_gesis_format_csv.py
+++ import_gesis_format_csv.py
@@ -125,7 +125,7 @@ def importer_gesis_vers_dhis2(tablename, filedbname, ListNomColonneInutile):
     dataSetID = browse_json_array(tablename, uid_dataset)
     iNombreLigne = 0
     for index, row in dfexcel.iterrows():
-        if(index > 284385):
+        if(index > 285334):
             if iNombreLigne != 10:
                 codegesisfs = int(dfexcel.loc[index, 'cCodeStru'])
                 ou_uid = get_uid_ou_dhis2(codegesisfs)
@@ -181,7 +181,7 @@ def importer_gesis_vers_dhis2(tablename, filedbname, ListNomColonneInutile):
             print("================================================================================")
             print("================================================================================")
             print("================================================================================")
-            iNombreLigne +=1
+            
 
         else:
             continue

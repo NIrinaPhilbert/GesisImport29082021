9192b27536904af9aa7decdb5754eb07a6dff723
NIrinaPhilbert <randriamiaranirina@gmail.com>
2021-09-14 09:54:36 +0300

    modification delay 0.00125

--- import_gesis_format_csv.py
+++ import_gesis_format_csv.py
@@ -128,7 +128,7 @@ def importer_gesis_vers_dhis2(tablename, filedbname, ListNomColonneInutile):
     for index, row in dfexcel.iterrows():
         # apres 520079e ligne
         # if(639357+24 == 639381, 21014 à 10 09 2021 à 08h41):
-        if (index > 13413):
+        if (index > 14419):
             if iNombreLigne != 5000:
                 codegesisfs = int(dfexcel.loc[index, 'cCodeStruct'])
                 ou_uid = get_uid_ou_dhis2(codegesisfs)
@@ -163,7 +163,7 @@ def importer_gesis_vers_dhis2(tablename, filedbname, ListNomColonneInutile):
                             if (de_uid != "" and len(de_uid) == 11 and coc_uid != "" and len(coc_uid) == 11):
                                 submit(de_uid, coc_uid, dataSetID, ou_uid, zPeriode, str(value_colonne))
                                 #time.sleep(0.00125)
-                                time.sleep(0.000625)
+                                time.sleep(0.00125)
 
                         else:
                             continue
@@ -172,7 +172,7 @@ def importer_gesis_vers_dhis2(tablename, filedbname, ListNomColonneInutile):
                     iNombreLigne += 1
                     print("iNombre ligne =" + str(iNombreLigne))
                     # time.sleep(0.00125)
-                    time.sleep(0.000625)
+                    time.sleep(0.00125)
 
                 else:
                     continue

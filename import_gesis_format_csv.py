7103125bfb355adff57e0b74c54b9625f75f9a31
NIrinaPhilbert <randriamiaranirina@gmail.com>
2021-10-27 11:40:19 +0300

    envoi file import tab11

--- import_gesis_format_csv.py
+++ import_gesis_format_csv.py
@@ -128,7 +128,7 @@ def importer_gesis_vers_dhis2(tablename, filedbname, ListNomColonneInutile):
     for index, row in dfexcel.iterrows():
         # apres 520079e ligne
         # if(639357+24 == 639381, 21014 à 10 09 2021 à 08h41):
-        if (index > 77450):
+        if (index > 0):
             if iNombreLigne != 5000:
                 codegesisfs = int(dfexcel.loc[index, 'cCodeStru'])
                 ou_uid = get_uid_ou_dhis2(codegesisfs)
@@ -294,8 +294,9 @@ ListNomColonneInutileTab14 = "cAnnee cCodeNiv cCodeStru cPeriode cTypeRapport"
 ListNomColonneInutileTab12 = "cAnnee cCodeNiv cCodeStru cPeriode cTypeRapport"
 ListNomColonneInutileTab10= "cAnnee cCodeNiv cCodeStru cPeriode cTypeRapport"
 ListNomColonneInutileTab9= "cAnnee cCodeNiv cCodeStru cPeriode cTypeRapport"
+ListNomColonneInutileTab11= "cAnnee cCodeNiv cCodeStru cPeriode cTypeRapport"
 # filedbname = 'Tab3TestErreur1.csv'
-filedbname = 'TAB_9_CPN_CSV.csv'
-Tablename = 'tRM_CSB_CONS_PRENAT'
+filedbname = 'TAB11_CSV_PEV.csv'
+Tablename = 'tRM_CSB_PEV_Enfants'
 # print(os.environ.get('w_param'))
-importer_gesis_vers_dhis2(Tablename, filedbname, ListNomColonneInutileTab9)
+importer_gesis_vers_dhis2(Tablename, filedbname, ListNomColonneInutileTab11)

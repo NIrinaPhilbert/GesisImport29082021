0859e19b2dc27fa3de4cb2550d1efc07254b91d3
NIrinaPhilbert <randriamiaranirina@gmail.com>
2022-01-04 13:13:59 +0300

    tab13 na et insert tab 13

--- import_gesis_format_csv.py
+++ import_gesis_format_csv.py
@@ -303,7 +303,7 @@ ListNomColonneInutileTab18= "cAnnee cCodeNiv cCodeStru cPeriode cTypeRapport"
 ListNomColonneInutileTab8= "cAnnee cCodeNiv cCodeStru cPeriode cTypeRapport"
 ListNomColonneInutileTab13= "cAnnee cCodeNiv cCodeStru cPeriode cTypeRapport"
 # filedbname = 'Tab3TestErreur1.csv'
-filedbname = 'TAB12_NA_CSVv2.csv'
-Tablename = 'tRM_CSB_PF'
+filedbname = 'TAB13 NA VF CSV.csv'
+Tablename = 'tRM_CSB_DENTISTERI'
 # print(os.environ.get('w_param'))
-importer_gesis_vers_dhis2(Tablename, filedbname, ListNomColonneInutileTab12)
+importer_gesis_vers_dhis2(Tablename, filedbname, ListNomColonneInutileTab13)

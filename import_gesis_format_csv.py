29233eacd32d389fa3e2f98f9877eaa9eda5dc99
NIrinaPhilbert <randriamiaranirina@gmail.com>
2022-01-04 08:45:47 +0300

    debut import tab12 CSV

--- import_gesis_format_csv.py
+++ import_gesis_format_csv.py
@@ -30,7 +30,7 @@ def browse_json_array(label, mylist):
 def get_uid_ou_dhis2(vcodegesis):
     uiddhis2 = ""
 
-    csv_file = open('metadatagesis/fsnavftab_3_4_8_9.csv', 'r')
+    csv_file = open('metadatagesis/fsnavftab_3_4_8_9_12.csv', 'r')
     csv_reader = csv.DictReader(csv_file)
     for line in csv_reader:
         codegesis = line['CODE_GESIS']
@@ -303,7 +303,7 @@ ListNomColonneInutileTab18= "cAnnee cCodeNiv cCodeStru cPeriode cTypeRapport"
 ListNomColonneInutileTab8= "cAnnee cCodeNiv cCodeStru cPeriode cTypeRapport"
 ListNomColonneInutileTab13= "cAnnee cCodeNiv cCodeStru cPeriode cTypeRapport"
 # filedbname = 'Tab3TestErreur1.csv'
-filedbname = 'TAB11_NA_CSV.csv'
-Tablename = 'tRM_CSB_PEV_Enfants'
+filedbname = 'TAB12_NA_CSV.csv'
+Tablename = 'tRM_CSB_PF'
 # print(os.environ.get('w_param'))
-importer_gesis_vers_dhis2(Tablename, filedbname, ListNomColonneInutileTab11)
+importer_gesis_vers_dhis2(Tablename, filedbname, ListNomColonneInutileTab12)
